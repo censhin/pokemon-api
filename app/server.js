d9e0997e5d429be57bc04eda833abef4bef9160d
censhin <censhin@gmail.com>
2016-05-20 08:15:24 -0500

    refactor(server, services): add logging in server to show what mongodb instance it's connected to, and add error handling for null values

--- app/server.js
+++ app/server.js
@@ -8,7 +8,6 @@ var port = 8000;
 var host = '172.17.0.2';
 var dbPort = '27017';
 var url = 'mongodb://' + host + ':' + dbPort + '/pokedex';
-console.log(url);
 
 routes(server);
 
@@ -17,6 +16,7 @@ db.connect(url, function(err, database) {
         console.log('Unable to connect to MongoDB');
         process.exit(1);
     } else {
+        console.log('Connected to MongoDB at %s', url);
         server.listen(port, function() {
             console.log('Server listening on port %s', port);
         });
--- app/services.js
+++ app/services.js
@@ -2,12 +2,20 @@ var dao = require('./daos.js');
 
 exports.get = function(next) {
     dao.get(function(err, result) {
-        next(err, { pokemon: result });
+        if(result) {
+            next(err, { pokemon: result });
+        } else {
+            next(err, { pokemon: [] });
+        }
     });
 }
 
 exports.getById = function(id, next) {
     dao.getById(id, function(err, result) {
-        next(err, result);
+        if(result) {
+            next(err, result);
+        } else {
+            next(err, {});
+        }
     });
 }
